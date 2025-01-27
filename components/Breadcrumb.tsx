1fb6cbaa28c14884aa12865e3e9d6f0bdbad677b
takushi <takutakushii@gmail.com>
2025-01-27 12:55:48 +0900

    first commit

---
+++ components/Breadcrumb.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const Breadcrumb = () => {
+  const router = useRouter()
+  let joinedPath = ''
+  console.log('Breadcrumbでパス取得：', router.asPath)
+
+  return (
+    <>
+      {router.asPath.split('/').map((path, index) => {
+        console.log('path', path)
+        if (path) {
+          joinedPath += path + '/'
+          console.log('joinedPath：', joinedPath)
+
+          return (
+            <Link key={index} href={`/${joinedPath}`}>
+              <a className="text-gray-500 hover:text-gray-600">
+                <span className="text-gray-500 mx-2">/</span> {path}
+              </a>
+            </Link>
+          )
+        }
+      })}
+    </>
+  )
+}
+
+export default Breadcrumb
---
+++ pages/articles/[slug].tsx
@@ -0,0 +1,76 @@
+import {
+  GetServerSideProps,
+  GetStaticPaths,
+  GetStaticProps,
+  NextPage,
+} from 'next'
+import React from 'react'
+import ArticleMeta from '../../components/ArticleMeta'
+import Layout from '../../components/Layout'
+import { ArticleProps, PageType, Params } from '../../types/types'
+import { fetchBlocksByPageId, fetchPages } from '../../utils/notion'
+import NotionBlocks from 'notion-block-renderer'
+import { sampleCards } from '../../utils/sample'
+import { getText } from '../../utils/property'
+import Block from '../../components/Block'
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { results } = await fetchPages({})
+  const paths = results.map((page: any) => {
+    return {
+      params: {
+        slug: getText(page.properties.slug.rich_text),
+      },
+    }
+  })
+  return {
+    paths: paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log('getServerSideProps pages/articles/[slug].tsx　コール')
+  const { slug } = ctx.params as Params
+
+  const { results } = await fetchPages({ slug: slug })
+  console.log('results: ', results)
+  const page = results[0]
+  const pageId = page.id
+  const { results: blocks } = await fetchBlocksByPageId(pageId)
+  return {
+    props: {
+      page: page,
+      blocks: blocks,
+    },
+    revalidate: 10,
+  }
+}
+
+const Article: NextPage<ArticleProps> = ({ page, blocks }) => {
+  // console.log('page', page)
+  // console.log('blocks', blocks)
+
+  return (
+    <Layout>
+      <article className="w-full">
+        {/* meta section */}
+        <div className="my-12">
+          <ArticleMeta page={page} />
+        </div>
+        {/* article */}
+        {/* <div className="my-12">
+          {blocks.map((block, index) => (
+            <Block key={index} block={block} />
+          ))}
+        </div> */}
+
+        <div className="my-12">
+          <NotionBlocks blocks={blocks} isCodeHighlighter={true} />
+        </div>
+      </article>
+    </Layout>
+  )
+}
+
+export default Article
