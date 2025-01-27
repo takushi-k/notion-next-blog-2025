bc8336f4b59fb00f698eef7f3ae22c6657d14fdf
takushi <takutakushii@gmail.com>
2025-01-27 13:27:09 +0900

    内容少し更新

--- site.config.ts
+++ site.config.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
-  title: 'Notion Next Blog',
-  twitterUrl: '/', // ご自身の値に書き換えてください
+  // title: 'Notion Next Blog',
+  title: 'Blogs',
+  twitterUrl: '', // ご自身の値に書き換えてください
   instagramUrl: '', // ご自身の値に書き換えてください
-  githubUrl: '/', // ご自身の値に書き換えてください
+  githubUrl: '', // ご自身の値に書き換えてください
 }
